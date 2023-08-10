77addd7ad3e65d85c5e5438c5b4f694c1a2d6f4a
akinyeke micheal <akinyekemicheal11@gmail.com>
2023-08-10 13:00:41 -0700

    using puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
+ * main - Entry point
  *
- * Description: This program prints a message and some stderr messages.
+ * Description: Prints a message to stdout.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts(" /\"Programming is like building a multilingual puzzle");
-	fputs("[stderr]: [Anything]\n", stderr);
-	fputs("(0 chars long)\n", stderr);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
