1ae5c537fa236b9f3788fc1c96f1e9080a7ffd77
akinyeke micheal <akinyekemicheal11@gmail.com>
2023-08-10 13:32:34 -0700

    bit size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints the size of various types.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
+	printf("Size of int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of float: %zu byte(s)\n", sizeof(float));
+	printf("Size of double: %zu byte(s)\n", sizeof(double));
+	printf("Size of long double: %zu byte(s)\n", sizeof(long double));
+
+	return (0);
+}
+
