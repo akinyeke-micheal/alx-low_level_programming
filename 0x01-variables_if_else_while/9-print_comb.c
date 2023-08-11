0c112d3bda66aa8810f2a14ed2dff9e9e86c98f2
akinyeke micheal <akinyekemicheal11@gmail.com>
2023-08-11 09:07:50 -0700

    gaddition in order

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints single-digit numbers separated by a comma and a space,
+ *              and terminates the line with a '$' character.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('$');
+
+	return (0);
+}
+
