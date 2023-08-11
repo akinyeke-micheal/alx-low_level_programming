35986ca5980a557a9ee738afcef7c7535b5ca3d4
akinyeke micheal <akinyekemicheal11@gmail.com>
2023-08-11 06:54:07 -0700

    digit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+
+	return (0);
+}
+
