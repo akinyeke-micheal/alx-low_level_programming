7c59142bb7023aac33f0b389c50fff0283c454a5
akinyeke micheal <akinyekemicheal11@gmail.com>
2023-08-15 09:11:03 -0700

    meta alphabets

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,7 @@
 #include "main.h"
 
 /**
- * main - Entry point of the program 
- * Return: Always 0 (Success)
+ * print_alphabet - print all alphabet in lowercase
  */
 
 void print_alphabet(void)
@@ -11,6 +10,7 @@ void print_alphabet(void)
 
 	for (letter = 'a'; letter <= 'z'; letter++)
 		_putchar(letter);
+
 	_putchar('\n');
 }
 
