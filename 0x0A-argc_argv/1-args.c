7acaee13ac5b48104fed55d0d1988ca408508f48
akinyeke micheal <akinyekemicheal11@gmail.com>
2023-09-04 06:48:34 -0700

    command line argument take 4

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+  * main - entry point
+  * @argc: cla1
+  * @argv: cla2
+  * Return: 0 on success else non zero
+  */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+		printf("%d\n", argc);
+	return (0);
+}
